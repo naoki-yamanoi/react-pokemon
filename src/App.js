287554a92978cad5490dd91438ba216dffd7a5a1
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-05-04 20:37:24 +0900

    １つ１つのポケモンデータ取得

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータ取得
       const res = await getAllPokemon(initialURL);
-      console.log(res);
+
+      loadPokemon(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    const _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        const pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+
+    setPokemonData(_pokemonData);
+  }
 
   return (
     <div className="App">
