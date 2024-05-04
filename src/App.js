ee03834aed2add70a87cde95d541a7e72d0e3922
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-05-04 20:04:45 +0900

    ポケモンデータ取得api追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
   return (
     <div className="App"></div>
   );
